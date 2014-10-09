a04e8268ae015f71eea11634101827ee7b43f199
ety001 <ety001@domyself.me>
2014-10-10 05:23:26 +0800

    comment complete

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -2,8 +2,46 @@ AV.initialize("tg7xkgxtxp6k1z28883mwkzv8bfriqtfot4ln8dbi01wtjmv", "vpmzwg2ttv276
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
+  var url = parseURL(tab.url);
+  var temp = canPublish(url);
+  if(!temp) {
+     return;
+  }
   var encode_url = encodeURIComponent(tab.url);
+  var title = encodeURIComponent(tab.title);
   chrome.windows.create(
-    {url: "main.html?" + encode_url, type: "popup", width: 400, height: 600}
+    {url: "main.html?" + encode_url + '#' + title , type: "popup", width: 400, height: 675}
   );
 });
+
+
+
+function parseURL(url) {
+  var result = {};
+  var match = url.match(
+      /^([^:]+):\/\/([^\/:]*)(?::([\d]+))?(?:(\/[^#]*)(?:#(.*))?)?$/i);
+  if (!match)
+    return result;
+  result.scheme = match[1].toLowerCase();
+  result.host = match[2];
+  result.port = match[3];
+  result.path = match[4] || "/";
+  result.fragment = match[5];
+  return result;
+}
+
+function canPublish(url) {
+  var path = url.path.split('/');
+  switch(url.host) {
+    case 'www.demohour.com':
+      if(path[1] !== 'projects') {
+        alert('暂不支持该页面吐槽');
+        return false;
+      }
+      break;
+    default:
+      alert('暂不支持该网站吐槽');
+      return false;
+  }
+  return true;
+}
--- chrome-extension/js/main.js
+++ chrome-extension/js/main.js
@@ -1,18 +1,25 @@
 $(function(){
     AV.initialize("tg7xkgxtxp6k1z28883mwkzv8bfriqtfot4ln8dbi01wtjmv", "vpmzwg2ttv276mk4ea7666fz0j62ov2gver8xdb47m8vigpj");
-    var BigBrain = AV.Object.extend("BigBrain");
+    var item_num = 10;
+    var BigBrainComments = AV.Object.extend("BigBrain_comments");
+    var BigBrainList = AV.Object.extend("BigBrain_list");
+    var url = window.location.search.replace('?','');
+    var title = decodeURIComponent( window.location.hash.replace('#','') ).replace('点名时间 - ','');
+    var url_hash = $.md5(url);
 
+    //读取评论
+    read_items(0);
+
+    //添加评论
     $('#submit_comment').click(function(){
+        $('.alert').addClass('hide').html('');
         var comment_text = $('#comment_input').val();
         if(!comment_text) {
             console.log('no comment');
-            $('.alert').addClass('alert-error').html('您还没有吐槽吧~~');
+            $('.alert').removeClass('hide').html('您还没有吐槽吧~~');
             return;
         }
-        var new_comment = new BigBrain();
-        var url = window.location.search;
-        url = url.replace('?','');
-        var url_hash = $.md5(url);
+        var new_comment = new BigBrainComments();
         
         new_comment.save(
             {
@@ -22,13 +29,70 @@ $(function(){
             },
             {
                 success: function(obj) {
-                    console.log(obj);
-                    alert("AVOS Cloud works!");
+                    var list_query = new AV.Query(BigBrainList);
+                    list_query.equalTo('url_hash',url_hash);
+                    list_query.find({
+                        success: function(res) {
+                            //add list
+                            if(res.length==0){
+                                var new_list_item = new BigBrainList();          
+                                new_list_item.save(
+                                    {title:title, url_hash:url_hash}
+                                );
+                            }
+                        }
+                    });
+                    var html_list = '<div class="well well-small success"><h5 class="media-heading">';
+                    html_list += comment_text;
+                    html_list += '</h5></div>';
+                    $('#list').prepend(html_list);
+                    $('#comment_input').val('');
+                    alert('吐槽成功');
                 }
             }
         );
-
     });
 
-    //var query = new AV.Query(BigBrain);
+    //获取更多
+    $('#add_more').click(function(){
+        read_items( parseInt($('#add_more').attr('num')) );
+    });  
+
+    function read_items(num) {
+        var list_query = new AV.Query(BigBrainComments);
+        var skip_num = num * item_num;
+        list_query.equalTo('url_hash', url_hash);
+        list_query.skip(skip_num);
+        list_query.limit(item_num);
+        list_query.descending('createdAt');
+        list_query.find()
+            .then(function(res){
+                if(res.length>0) {
+                    //第一次读取，并且数量等于每页的条数，则显示加载更多按钮
+                    if ( num == 0 && res.length == item_num) {
+                        $('#add_more').removeClass('hide');
+                    }
+                    var html_list = '';
+                    for(var i in res) {
+                        var obj = res[i];
+                        html_list += '<div class="well well-small success"><h5 class="media-heading">';
+                        html_list += obj.get('comment');
+                        html_list += '</h5></div>';
+                    }
+                    $('#list').append(html_list);
+                    var page_num = parseInt($('#add_more').attr('num'));
+                    $('#add_more').attr('num',page_num+1);
+                } else {
+                    if ( num == 0 ) {
+                        $('#list').prepend('<div class="well well-small success"><h5 class="media-heading">暂无吐槽</h5></div>');
+                    } else {
+                        $('#add_more').html('没有吐槽啦~').unbind('click');
+                    }   
+                }
+                if(res.length<item_num) {
+                    $('#add_more').html('没有吐槽啦~').unbind('click');
+                }
+            }
+        );
+    }
 });
